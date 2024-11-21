1bb9d591f2fc42d8a5c9a75fd7b00a098e4f5154
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2024-11-22 01:01:20 +0530

    Update and rename main.js to app.json

--- main.js
+++
@@ -1,8 +0,0 @@
-
-//base by CYBER UDAYIPP 
-//re-upload? recode? copy code? give credit ya :)
-//YouTube: @cyberudayipp
-//GitHub: @cyberudayipp
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@cyberudayipp
-
-      
