d1ec5a1da98b51a58544e6fe79d70475dc7650aa
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2024-11-25 16:31:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
+global.github = 'https://github.com/CYBERUDAYIPP/ZROXY-MD'
 global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
-global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
+global.gurl = 'https://instagram.com/cyber_udayipp' // add your username
+global.sudo = process.env.SUDO || '918129540560'
+global.devs = '918129540560';
+global.website = 'https://github.com/CYBERUDAYIPP/ZROXY-MD' 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'ð’ðžðœð¤ð­ð¨ð« ðð¨ð­ð­ð¨' : process.env.BOT_NAME,
