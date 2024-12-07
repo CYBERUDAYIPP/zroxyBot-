7d765b2f89aaed47280a1d59a4e3c32ee3304b00
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2024-12-07 16:23:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.gurl = 'https://instagram.com/cyber_udayipp' // add your username
 global.sudo = process.env.SUDO || '918129540560'
 global.devs = '918129540560';
 global.website = 'https://github.com/CYBERUDAYIPP/ZROXY-MD' 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://files.catbox.moe/4taoyw.jpeg'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'ð’ðžðœð¤ð­ð¨ð« ðð¨ð­ð­ð¨' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'cyberudayipp' : process.env.OWNER_NAME,
@@ -34,7 +34,7 @@ module.exports = {
   antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
   antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
   BRANCH: 'main', 
-  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? 'Iam Alive' : process.env.ALIVE_MESSAGE,
+  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? 'Iam Alive☠️' : process.env.ALIVE_MESSAGE,
   autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
   heroku:  process.env.heroku === undefined ? false : process.env.heroku,
